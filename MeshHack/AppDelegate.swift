6c1da0f64f7f7aba5cfbce1dbb872864faf16b59
takashi hatakeyama <takashi.hatakeyama@gmail.com>
2016-10-11 16:54:06 +0900

    Initial Commit

---
+++ MeshHack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MeshHack
+//
+//  Created by Takashi Hatakeyama on 2016/10/11.
+//  Copyright © 2016年 esm. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MeshHack/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MeshHack
+//
+//  Created by Takashi Hatakeyama on 2016/10/11.
+//  Copyright © 2016年 esm. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
